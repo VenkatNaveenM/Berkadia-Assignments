a2576e5cfe555e0e3d8c486260d2f611b4936d46
VENKAT NAVEEN MEDARAMETLA <41520213+VenkatNaveenM@users.noreply.github.com>
2018-11-20 21:10:31 +0530

    Update Inheritance.js

--- Week-3/NodeJs/Inheritance.js
+++ Week-3/NodeJs/Inheritance.js
@@ -11,7 +11,6 @@ var inheritsFrom = function (child, parent) {
 };
 var ClassB = function() {
     this.name = "class B";
-    this.surname = "I'm the child";
 }
 
 inheritsFrom(ClassB, ClassA);
@@ -21,21 +20,3 @@ ClassB.prototype.print = function() {
     ClassA.prototype.print.call(this);
     console.log(this.surname);
 }
-var ClassC = function () {
-    this.name = "class C";
-    this.surname = "I'm the grandchild";
-}
-
-inheritsFrom(ClassC, ClassB);
-
-ClassC.prototype.foo = function() {
-    
-}
-
-ClassC.prototype.print = function () {
-    ClassB.prototype.print.call(this);
-    console.log("Sounds like this is working!");
-}
-
-var c = new ClassC();
-c.print();
