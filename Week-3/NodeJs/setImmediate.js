c9e47c3bc8620948a35f25f9e9a4cd4b88b96169
VENKAT NAVEEN MEDARAMETLA <41520213+VenkatNaveenM@users.noreply.github.com>
2018-11-20 22:26:06 +0530

    Add files via upload

---
+++ Week-3/NodeJs/setImmediate.js
@@ -0,0 +1,19 @@
+/* setImmediate() will execute code at the end of the current event loop cycle. 
+This code will execute after any I/O operations in the current event loop and before
+ any timers scheduled for the next event loop. This code execution could be thought of 
+ as happening "right after this", meaning any code following the setImmediate() function
+  call will execute before the setImmediate() function argument.    */
+
+
+console.log('Before immediate');
+
+setImmediate((arg) => {
+  console.log(`Executing immediate: ${arg}`);
+}, 'so immediate');
+
+console.log('After immediate');
+
+C:\Users\Venkat Naveen\Berkadia\Week 3\NodeJs>node setImmediate.js
+Before immediate
+After immediate
+Executing immediate: so immediate
