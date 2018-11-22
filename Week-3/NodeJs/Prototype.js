c11d0f7463b52bc5ae47e17c6ff011ec929d703d
VENKAT NAVEEN MEDARAMETLA <41520213+VenkatNaveenM@users.noreply.github.com>
2018-11-22 19:42:16 +0530

    Update Prototype.js

--- Week-3/NodeJs/Prototype.js
+++ Week-3/NodeJs/Prototype.js
@@ -1,3 +1,4 @@
+/*
 When a function is created in Java Script, JavaScript engine addsa prototype property to the function.
 This prototype property is called Object has a constructor property by default.
 
@@ -5,6 +6,7 @@ Advantages of prototype:
 
 No matter how many objects you create, functions are loaded only once in to the memory.
 Allow you to override functions if required.
+*/
 
 function Employee(name){
     this.name=name;
@@ -17,8 +19,10 @@ var e2=new Employee("Naveen");
 console.log("E1=",e1.getName());
 console.log("E2=",e2.getName());
 
+/*
 output:
 
 C:\Users\Venkat Naveen\Berkadia\Week 3\NodeJs>node Prototype.js
 E1= Venkat
 E2= Naveen
+*/
