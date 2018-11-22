4ed57d447853e167f76464eb0bf7727551ce8ca3
VENKAT NAVEEN MEDARAMETLA <41520213+VenkatNaveenM@users.noreply.github.com>
2018-11-22 22:20:34 +0530

    Update Closure.js

--- Week-3/NodeJs/Closure.js
+++ Week-3/NodeJs/Closure.js
@@ -1,17 +1,62 @@
-/* A closure is the combination of a function and the lexical environment
- within which that function was declared.*/
+/*
+A closure is an inner function that has access to the outer function’s variables in addition to it's own variables and global variables. 
+The inner function has access not only to the outer function’s variables, but also to the outer function’s parameters.
+You create a closure by adding a function inside another function.
+*/
 
 
-function init() {
-    var name = 'Venkat Naveen';
-    function displayName() {
-      console.log(name);    
+//JavaScript Closure Example 
+
+function addNumbers(firstNumber, secondNumber) 
+{
+    var returnValue = "Result is : ";
+    // This inner function has access to the outer function's variables & parameters
+    function add() 
+    {
+        return returnValue + (firstNumber + secondNumber);
     }
-    displayName();    
-  }
-  init();
+    return add();
+}
 
+var result = addNumbers(10, 20);
+document.write(result);
 
-/*  out put:
-C:\Users\Venkat Naveen\Berkadia\Week 3\NodeJs>node Closure.js
-Venkat Naveen   */
+//Output : Result is : 30
+
+//The following code Returns the inner function expression
+
+function addNumbers(firstNumber, secondNumber) 
+{
+    var returnValue = "Result is : ";
+    function add() 
+    {
+        return returnValue + (firstNumber + secondNumber);
+    }
+    // We removed the parentheses. This will return the inner function expression without executing it.
+    return add;
+}
+
+// addFunc will contain add() function (inner function) expression.
+var addFunc = addNumbers(10, 20);
+// call the addFunc() function and store the return value in result variable
+var result = addFunc();
+
+document.write(result);
+
+//Returning and executing the inner function
+
+function addNumbers(firstNumber, secondNumber) 
+{
+    var returnValue = "Result is : ";
+    function add() 
+    {
+        return returnValue + (firstNumber + secondNumber);
+    }
+    // We removed the parentheses. This will return the inner function add() expression without executing it.
+    return add;
+}
+
+// This returns add() function (inner function) definition and executes it. Notice the additonal parentheses.
+var result = addNumbers(10, 20)();
+
+document.write(result);
