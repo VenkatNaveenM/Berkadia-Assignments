a194973f6ea3793cda6ae4f3106c6c5efed44f58
VENKAT NAVEEN MEDARAMETLA <41520213+VenkatNaveenM@users.noreply.github.com>
2018-11-22 19:38:27 +0530

    Update Inheritance.js

--- Week-3/NodeJs/Inheritance.js
+++ Week-3/NodeJs/Inheritance.js
@@ -1,3 +1,10 @@
+/*In object-oriented programming, inheritance is the concept that when a class of objects is defined, 
+any subclass that is defined can inherit the definitions of one or more general classes.
+This means for the programmer that an object in a subclass need not carry its own definition of data
+and methods that are generic to the class (or classes) of which it is a part. This not only speeds up program development;
+it also ensures an inherent validity to the defined subclass object (what works and is consistent about the class will also 
+work for the subclass).*/
+
 var ClassA = function() {
     this.name = "class A";
 }
@@ -20,3 +27,8 @@ ClassB.prototype.print = function() {
     ClassA.prototype.print.call(this);
     console.log(this.surname);
 }
+
+/* Out Put
+C:\Users\Venkat Naveen\Berkadia\Week 3\NodeJs>node Inheritance.js
+class A
+class B  */
